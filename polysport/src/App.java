4eb5cdec6b3e1cc4945b69667bec9313588faa18
JinxYTW <tyohann974@gmail.com>
2024-05-13 14:23:08 +0200

    Requete SQL simple

--- polysport/src/App.java
+++ polysport/src/App.java
@@ -1,19 +1,31 @@
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 import com.mysql.cj.jdbc.Driver;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try{
-        Class.forName("com.mysql.cj.jdbc.Driver"); // Load the MySQL driver
-        Connection connection = DriverManager.getConnection(
-        "jdbc:mysql://localhost:3306/poly_sports",
-        "root",
-        ""
-        );
-        System.out.println("Connected to the database");
-    }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Load the MySQL driver
+
+            Connection myConnection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/poly_sports",
+                "root",
+                ""
+            ); // Connect to the database
+
+            System.out.println("Connected to the database");
+
+            java.sql.Statement myStatement = myConnection.createStatement();
+            ResultSet myResultSet = myStatement.executeQuery("SELECT * FROM sport");
+
+            while(myResultSet.next()){
+                
+                System.out.println(myResultSet.getString("name")+"|"+myResultSet.getInt("required_participants"));
+            }
+        }
     catch(Exception e){
         System.out.println("Error: " + e);
     }
